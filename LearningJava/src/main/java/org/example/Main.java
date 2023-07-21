431e3ae8ab8a11cf7492dee3f10ec2a473864712
Sviatoslav Binovskyi <santonase@gmail.com>
2023-07-21 13:14:13 +0300

    Math.pow, math.round

---
+++ LearningJava/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+//Возведите число «1.4234» в степень 3. Округлите число к большему и выведите его на экран.
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Math.round(Math.pow(1.4234, 3)));
+
+    }
+}
