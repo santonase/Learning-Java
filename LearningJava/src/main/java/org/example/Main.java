7fa13d452527bf7cd1e2679dd7d7c0ef21215137
Sviatoslav Binovskyi <santonase@gmail.com>
2023-07-24 11:38:51 +0300

    Scanner and cycel do-while

--- LearningJava/src/main/java/org/example/Main.java
+++ LearningJava/src/main/java/org/example/Main.java
@@ -4,14 +4,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Enter number 7");
+        int number;
         Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-        while (number != 7) {
-            System.out.println("Enter number 7 again");
+        do {
+            System.out.println("Enter number 7");
             number = scanner.nextInt();
-        }
+        } while (number != 7);
         System.out.println("Number is 7 !!!");
     }
 }
