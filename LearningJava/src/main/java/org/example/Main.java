ad94011f50545722f470a2b13f94748d7a55f7b2
Sviatoslav Binovskyi <santonase@gmail.com>
2023-07-25 12:04:28 +0300

    scanner and random number between min/max

--- LearningJava/src/main/java/org/example/Main.java
+++ LearningJava/src/main/java/org/example/Main.java
@@ -1,19 +1,24 @@
 package org.example;
 
-
+import java.util.Random;
 import java.util.Scanner;
+
+import static java.lang.System.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Your name: ");
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
 
-        System.out.println(" Your age: ");
-        int age = scanner.nextInt();
+        Scanner scanner = new Scanner(in);
+        out.println("Your min number: ");
+        int min = scanner.nextInt();
+        out.println("Your max number: ");
+        int max = scanner.nextInt();
+        Random r = new Random();
 
-        System.out.printf("Name: %s, Age: %d", name, age);
-
+        for(int i = 0; i < 10; i++) {
+            int result = r.nextInt(max - min) + min;
+            out.println(result);
+        }
     }
 }
