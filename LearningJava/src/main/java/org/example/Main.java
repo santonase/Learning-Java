c1aeabf636291aa7204a41fe44dfb678920194c2
Sviatoslav Binovskyi <santonase@gmail.com>
2023-07-25 14:57:12 +0300

    if, else, switch

--- LearningJava/src/main/java/org/example/Main.java
+++ LearningJava/src/main/java/org/example/Main.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import java.util.Random;
 import java.util.Scanner;
 
 import static java.lang.System.*;
@@ -8,21 +7,57 @@ import static java.lang.System.*;
 public class Main {
     public static void main(String[] args) {
 
+        //Task-1:
+        Scanner scanner = new Scanner(in);
+        out.println("Enter your age: ");
+        int age = scanner.nextInt();
 
-        Scanner scanner = new Scanner(System.in);
+        if (age <= 13) {
+            out.println("You have not access");
+        } else if (age >= 14 && age <= 17) {
+            out.println("You have part access");
+        } else {
+            out.println("You have full access");
+        }
 
-        System.out.printf("min random: ");
-        int min = scanner.nextInt();
+        //TASK-2:
+        Scanner scanner1 = new Scanner(in);
+        out.println("Enter first number: ");
+        double firstNumber = scanner1.nextDouble();
+        out.println("Enter second number: ");
+        double secondNumber = scanner1.nextDouble();
+        out.println("Enter operator");
+        char operator = scanner1.next().charAt(0);
 
-        System.out.printf("Max random: ");
-        int max = scanner.nextInt();
+        switch (operator) {
+            case '+':
+            out.println(firstNumber + secondNumber);
+            break;
 
-        int ran = min +  (int)(Math.random()* max);
+            case '-':
+                out.println(firstNumber - secondNumber);
+                break;
 
-        System.out.println(ran);
-        out.println(min);
-        out.println(max);
+            case '*':
+                out.println(firstNumber * secondNumber);
+                break;
 
-        
+            case '/':
+                out.println(firstNumber / secondNumber);
+                break;
+
+            case '%':
+                out.println(firstNumber % secondNumber);
+                break;
+
+            default:
+                out.println("Error");
+        }
+
+
+
+
+
+
     }
 }
